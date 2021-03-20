753bb37564ccd22a3c55a42eee6299157c19dff7
Michael Ajwani <michael.ajwani@i22.de>
2021-03-20 14:17:50 +0100

    wip model MigrationState | Issue #3

--- lib/better_data_migration/models/migration_state.rb
+++ lib/better_data_migration/models/migration_state.rb
@@ -1,5 +1,30 @@
+# == Schema Information
+#
+# Table name: app_migrations
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  applied    :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 module BetterDataMigration
   class MigrationState < ActiveRecord::Base
+    class MigrationNotFoundError < StandardError; end
+
     self.table_name = BetterDataMigration::MIGRATION_TABLE_NAME
+
+    # alt: 2714, 2715, 2612, 2757
+    OK_SYMBOL = "\u2705".freeze
+    NOK_SYMBOL = "\u2757".freeze
+
+    attr_accessor :migration_class, :required
+
+    scope :ordered, -> { order(:id) }
+    scope :migrated, -> { where(applied: true).ordered }
+    scope :pending, -> { where(applied: false).ordered }
+
+    validates :name, presence: true
   end
 end
--- test/models/migration_state_test.rb
+++ test/models/migration_state_test.rb
@@ -1,11 +1,44 @@
 require 'test_helper'
 
 class MigrationStateTest < ActiveSupport::TestCase
-  setup do
-    @model = BetterDataMigration::MigrationState.new
-  end
+  describe 'Model' do
+    before do
+      @model_class = BetterDataMigration::MigrationState
+      @model = @model_class.new(name: 'test', applied: false)
+    end
 
-  def test_it_does_something_useful
-    assert true
+    it 'is a valid instance' do
+      assert @model.valid?
+      assert @model.save
+    end
+
+    it 'has constants for symbols' do
+      assert @model_class.const_defined? 'OK_SYMBOL'
+      assert @model_class.const_defined? 'NOK_SYMBOL'
+    end
+
+    describe 'Scopes' do
+      it 'has an ordered-scopes' do
+        assert_respond_to(@model_class, :ordered)
+      end
+
+      it 'has a migrated-scope' do
+        assert_empty @model_class.migrated
+
+        @model.update(applied: true)
+
+        refute_empty @model_class.migrated
+      end
+
+      it 'has a pending-scope' do
+        @model.update(applied: true)
+
+        assert_empty @model_class.pending
+
+        @model.update(applied: false)
+
+        refute_empty @model_class.pending
+      end
+    end
   end
 end
