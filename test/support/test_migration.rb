c192c1cc253a1b87882bdfe6c093297b754530ec
Michael Ajwani <michael.ajwani@i22.de>
2021-03-20 16:08:14 +0100

    added default-code to class TestMigration | Issue #3

--- test/support/test_migration.rb
+++ test/support/test_migration.rb
@@ -1,2 +1,20 @@
 class TestMigration < BetterDataMigration::Migration
+  DESCRIPTION = ''.freeze
+  UNATTENDED = true
+
+  def initialize
+    super
+  end
+
+  def setup
+    # Your setup-code
+  end
+
+  def rollback
+    # Your rollback-code
+  end
+
+  def summary
+    puts 'TestMigration migrated'
+  end
 end
