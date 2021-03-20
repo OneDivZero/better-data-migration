ded8e71ae8c1d520b47e59cbc93ba20350a40729
Michael Ajwani <michael.ajwani@i22.de>
2021-03-20 17:40:41 +0100

    extended path-handling for gem | Issue #3

--- lib/better_data_migration/config.rb
+++ lib/better_data_migration/config.rb
@@ -1,4 +1,19 @@
 module BetterDataMigration
+  GEM_NAME = 'better-data-migration'.freeze
   MIGRATION_TABLE_NAME = 'app_migrations'.freeze
   MIGRATION_PATH = 'db/data'.freeze
+
+  def self.root_path
+    Gem::Specification.find_by_name(GEM_NAME).gem_dir
+  end
+
+  # TODO: Checkout if this does work in app-context where the gem is used
+  def self.migration_files_path
+    current_root = rails_root_available? ? Rails.root.to_s : root_path
+    [current_root, '/', MIGRATION_PATH].join
+  end
+
+  def self.rails_root_available?
+    Object.const_defined?('Rails') && Rails.respond_to?(:root)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,9 +13,14 @@ require 'minitest/focus'
 require 'minitest/spec'
 require 'minitest/pride'
 
+require 'better_data_migration/config'
 require 'better_data_migration/migration'
 
 ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
 
 load File.dirname(__FILE__) + '/schema.rb'
 load File.dirname(__FILE__) + '/support/test_migration.rb'
+
+def gem_root
+  BetterDataMigration.root_path
+end
